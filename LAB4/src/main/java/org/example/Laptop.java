35d0b84bbadca5f964785bc21aa01fd8800d077a
Denan02 <dpoturak1@etf.unsa.ba>
2023-11-12 12:58:13 +0100

    Klase
    DodavanjeKlasa

---
+++ LAB4/src/main/java/org/example/Laptop.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Laptop {
+    private String brend, model, grafickaKartica, procesor;
+    private double cijena, velicinaEkrana;
+    private int ram, hdd, ssd;
+}
